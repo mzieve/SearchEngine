b42dcb207bb2e0fbb158010fba7b27de6ca90d68
CaptainComputer1 <115894622+CaptainComputer1@users.noreply.github.com>
2023-10-24 10:43:36 -0700

    New Project Milestone 2 Files

--- engine/querying/rankedquery.py
+++ engine/querying/rankedquery.py
@@ -1,3 +1,12 @@
+from engine.text import Preprocessing
+import math
 class RankedQuery:
     #define Ranked Querying here.
-    pass
+    def parse_query(query: str, preprocess: Preprocessing):
+        # Get each term in the query
+        proc_query = preprocess.process(query)
+        terms = proc_query.split(' ')
+        for term in terms:
+            #Find N (number of documents) and dft (document frequency of term).
+            #wqt = math.log(1 + (N/dft))
+            pass
